6f919f55029b562d2676b30d8ca1afd5ea804aca
angelgiovanna <aescobar2045@gmail.com>
2022-02-24 13:45:05 -0500

    in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskIdCounter = 0;
 
 
 var taskFormHandler = function(event) {
@@ -29,6 +30,8 @@ var createTaskEl = function(taskDataObj) {
 var listItemEl = document.createElement("li");
 listItemEl.className = "task-item";
 
+listItemEl.setAttribute("data-task-id", taskIdCounter);
+
 // create div to hold task info and add to list item
 var taskInfoEl = document.createElement("div");
 taskInfoEl.className = "task-info";
@@ -37,6 +40,8 @@ listItemEl.appendChild(taskInfoEl);
 
 // add entire list item to list
 tasksToDoEl.appendChild(listItemEl);
-}
+
+taskIdCounter++;
+};
 
 formEl.addEventListener("submit", taskFormHandler);
